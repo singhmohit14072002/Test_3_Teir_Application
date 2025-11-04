37420beebefcb1627bdb69a66130720d5f536c62
Mohit Singh <singhmohit14072002@gmail.com>
2025-11-04 13:21:33 +0530

    Initial commit - 3 tier app with Azure SQL

---
+++ BackEnd/db.js
@@ -0,0 +1,10 @@
+const dbConfig = {
+  user: 'your-sql-username',
+  password: 'your-sql-password',
+  server: 'your-sql-server.database.windows.net',
+  database: 'your-database-name',
+  options: {
+    encrypt: true
+  }
+};
+module.exports = dbConfig;
